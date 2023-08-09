0e8d363059af6c444e8ecb9dcf53afb0618a9e18
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-08-09 09:55:18 +0900

    임시 커밋

---
+++ src/test/java/com/sixesSense/recorder/review/command/application/controller/ReviewControllerTests.java
@@ -0,0 +1,8 @@
+package com.sixesSense.recorder.review.command.application.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ReviewControllerTests {
+
+}
