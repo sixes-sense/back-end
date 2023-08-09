000070f1c171978fba6e01a615a7ce4f4d49127e
psi5059 <psi5059@naver.com>
2023-08-09 20:50:58 +0900

    Report Entity 추가

---
+++ src/main/java/com/sixesSense/recorder/report/command/domain/aggregate/vo/ReporterVo.java
@@ -0,0 +1,20 @@
+package com.sixesSense.recorder.report.command.domain.aggregate.vo;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ReporterVo {
+
+    @Column(name = "member_no")
+    // 신고자
+    private Long reporter;
+
+}
