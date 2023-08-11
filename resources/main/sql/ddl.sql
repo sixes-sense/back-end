090739fd4c63bfde04f7db4dfb12ea0073f76907
bbbbooo <bbbbooo@users.noreply.github.com>
2023-08-11 01:12:41 +0000

    deploy: 1b89431796c0b7abe0ae2421c6caf5ae04fdbafa

---
+++ resources/main/sql/ddl.sql
@@ -0,0 +1,12 @@
+create table review (
+                        review_no bigint not null auto_increment,
+                        book_mark_cnt bigint,
+                        like_cnt bigint,
+                        report_cnt bigint,
+                        review_content varchar(255),
+                        review_date date,
+                        review_title varchar(255),
+                        member_no bigint,
+                        tag_no bigint,
+                        primary key (review_no)
+)
