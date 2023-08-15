7a9bf9a81fd2cb009a5b5b836af9dd7a74271420
taegeun-park <taegeun0525@gamil.com>
2023-08-15 13:58:22 +0900

    [feat] : Member Entity 생성
    BaseTimeEntity를 상속받았음
    Role.java => 멤버 권한 Enum

---
+++ src/main/java/com/sixesSense/recorder/member/command/domain/aggregate/entity/EnumType/Role.java
@@ -0,0 +1,10 @@
+package com.sixesSense.recorder.member.command.domain.aggregate.entity.EnumType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    ROLE_GUEST, ROLE_MEMBER
+}
--- src/main/java/com/sixesSense/recorder/member/command/domain/aggregate/entity/Member.java
+++ src/main/java/com/sixesSense/recorder/member/command/domain/aggregate/entity/Member.java
@@ -1,4 +1,55 @@
 package com.sixesSense.recorder.member.command.domain.aggregate.entity;
 
-public class Member {
+import com.sixesSense.recorder.AchievementsDomain.command.domain.common.entity.BaseTimeEntity;
+import com.sixesSense.recorder.member.command.domain.aggregate.entity.EnumType.Role;
+import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "Member_TB")
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_no")
+    @Comment("회원번호")
+    private Long id;
+
+    @NotBlank
+    @Comment("회원 이름")
+    private String name;
+
+    @Comment("회원 닉네임")
+    private String nickName;
+
+    @Comment("회원 주소")
+    private String address;
+
+    @NotBlank
+    @Comment("회원 이메일")
+    private String email;
+
+    @Comment("회원 핸드폰번호")
+    private String phoneNum;
+
+    @Enumerated(EnumType.STRING)
+    @Comment("회원 권한")
+    private Role role;
+
+    public Member(Long id, String name, String nickName, String address, String email, String phoneNum, Role role) {
+        this.id = id;
+        this.name = name;
+        this.nickName = nickName;
+        this.address = address;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.role = role;
+    }
 }
+
