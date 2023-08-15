7e7cc9f2020dcddf7ddd28d52030b3f4c66bf405
taegeun-park <taegeun0525@gamil.com>
2023-08-15 11:27:56 +0900

    [feat] : 패키지 구조 생성
    member 패키지 구조생성함

---
+++ src/main/java/com/sixesSense/recorder/member/command/domain/aggregate/entity/Member.java
@@ -0,0 +1,4 @@
+package com.sixesSense.recorder.member.command.domain.aggregate.entity;
+
+public class Member {
+}
---
+++ src/main/java/com/sixesSense/recorder/member/command/domain/repository/MemberRepository.java
@@ -0,0 +1,4 @@
+package com.sixesSense.recorder.member.command.domain.repository;
+
+public interface MemberRepository {
+}
