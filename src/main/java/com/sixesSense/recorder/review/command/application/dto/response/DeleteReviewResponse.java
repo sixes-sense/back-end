95b54abc7c415dfb5d06add623153855b93a277e
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-08-16 21:29:09 +0900

    refactor : 리뷰 dto 일부 추가

---
+++ src/main/java/com/sixesSense/recorder/review/command/application/dto/response/DeleteReviewResponse.java
@@ -0,0 +1,11 @@
+package com.sixesSense.recorder.review.command.application.dto.response;
+
+import com.sixesSense.recorder.review.command.application.dto.response.common.CommonReviewResponse;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DeleteReviewResponse<T> extends CommonReviewResponse {
+    private T data;
+}
