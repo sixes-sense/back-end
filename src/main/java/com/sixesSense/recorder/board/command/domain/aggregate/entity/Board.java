21c4d8ba60b8f26c69fbf4f7983ad055fbf4db4e
jinvvoo <jinvvookim@gmail.com>
2023-08-22 11:16:46 +0900

    [Feat] : board Entity 생성
    1. Entity 작성함 (1커밋)

--- src/main/java/com/sixesSense/recorder/board/command/domain/aggregate/entity/Board.java
+++ src/main/java/com/sixesSense/recorder/board/command/domain/aggregate/entity/Board.java
@@ -1,4 +1,35 @@
 package com.sixesSense.recorder.board.command.domain.aggregate.entity;
 
-public class Board {
+
+import com.sixesSense.recorder.common.entity.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "Board_TB")
+public class Board extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;            // 글 작성 번호
+
+    @Column(name = "title", length = 50)
+    private String title;       // 제목
+
+    @Column(name = "Content", columnDefinition = "TEXT", nullable = false)
+    private String content;     // 본문
+
+    @Column(name = "requester_id", nullable = false)
+    private long adminId;       // 작성자(관리자)
+
+    public Board(long id, String title, String content, long adminId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.adminId = adminId;
+    }
 }
