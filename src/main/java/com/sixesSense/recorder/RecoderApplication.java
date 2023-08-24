8debb45dab7d9b234112c61ae5992ca510fbe4ca
박성준 <94158097+myrhymetree@users.noreply.github.com>
2023-08-24 19:54:59 +0900

    [FEAT]: spring cloud config client

--- src/main/java/com/sixesSense/recorder/RecoderApplication.java
+++ src/main/java/com/sixesSense/recorder/RecoderApplication.java
@@ -1,13 +1,10 @@
 package com.sixesSense.recorder;
 
-import com.sixesSense.recorder.config.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableConfigurationProperties(MyConfig.class)
 @EnableJpaAuditing
 public class RecoderApplication {
 
